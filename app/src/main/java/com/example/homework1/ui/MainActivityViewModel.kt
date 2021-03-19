6232679473928f1293945bf68c4a285d89b3f6c1
Rukishido <jugodbwork@gmail.com>
2021-03-19 20:49:28 +0700

    fixed grade validation

--- app/src/main/java/com/example/homework1/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/example/homework1/ui/MainActivityViewModel.kt
@@ -27,12 +27,17 @@ class MainViewModel : ViewModel() {
     private fun isStudentFieldsValid(fields: List<String>): Boolean {
         return fields[0].matches("[a-zA-Z]{3,}".toRegex()) && //Name
                 fields[1].matches("[a-zA-Z]{3,}".toRegex()) && //Surname
-                fields[2].matches("[1]?[0-9]\\w".toRegex()) && //Grade
+                validateGrade(fields[2]) && //Grade
                 validateYear(fields[3]) // Year
     }
 
     private fun validateYear(yearString: String): Boolean {
         return yearString.matches("[1-2][0-9][0-9][0-9]".toRegex()) &&
                 yearString.toInt() in 1930..Calendar.getInstance().get(Calendar.YEAR)
+    }
+
+    private fun validateGrade(gradeString: String): Boolean {
+        return gradeString.matches("[1]?[0-9]\\D".toRegex()) &&
+                gradeString.replace("\\D".toRegex(), "").toInt() in 1..11
     }
 }
